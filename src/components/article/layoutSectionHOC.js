5a3b4dda5cfd0b79fe719497ef95dce66258eb37
mr.scrpt <mr.scrpt@gmail.com>
2023-05-11 22:03:57 +0300

    layout HOC refacroting with arrow chaining

--- src/components/article/layoutSectionHOC.js
+++ src/components/article/layoutSectionHOC.js
@@ -2,20 +2,19 @@ import React from 'react';
 import ArticleViewList from './articleViewList';
 import ArticleViewTable from './articleViewTable';
 
-function LayoutSectionHOC(Component) {
-  return (props) => {
+const LayoutSectionHOC = (Component) => (props) => (
+  <section
+    className='section'
+    style={{color: "red", padding: "150px 30px"}}
+  >
+    <h1>{props.titleSection}</h1>
+    <Component
+      {...props}
+    />
+  </section>
+);
 
-    return (
-      <section className='section' style={{color: "red", padding: "150px 30px"}}>
-        <h1>{props.titleSection}</h1>
-        <Component
-          {...props}
-        />
-      </section>
-    );
-  };
 
-}
 export const ArticleTableWithLayout = LayoutSectionHOC(ArticleViewTable);
 export const ArticleListWthLayout = LayoutSectionHOC(ArticleViewList);
 
